802733ceb58d38961e974104c61036bbed402d0e
unknown <haoyex@uci.edu>
2023-03-01 14:56:58 -0800

    some message about the change you made

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoadingIndicator from "./components/LoadingIndicator";
 import Landing from "./containers/Landing";
 import ErrorBoundary from "./containers/ErrorBoundary";
-
+import Wisdom_container from "./containers/Wisdom_container";
+import Register_container from "./containers/Register_container";
+import Register_categories_container from "./containers/Register_categories_container";
+import Question_answer_container from "./containers/Question_answer_container";
 function App() {
   const [isLoading, setIsLoading] = useState(false);
 
@@ -17,7 +20,8 @@ function App() {
         >
           <div>
             <Routes>
-              <Route path={`${process.env.PUBLIC_URL}/`} element={<Landing />}></Route>
+              <Route path={`${process.env.PUBLIC_URL}/`} element={<Register_container/>}></Route>
+              
             </Routes>
             <LoadingIndicator loading={isLoading} />
           </div>
