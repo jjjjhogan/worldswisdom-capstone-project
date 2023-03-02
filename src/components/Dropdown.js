8e5697d057fbb8ddcf64fdec6a4aa2fd137ba83e
John Hogan <jjhogan@uci.edu>
2023-03-01 18:14:14 -0800

    Adding Dropdown component

---
+++ src/components/Dropdown.js
@@ -0,0 +1,34 @@
+export default function Dropdown({
+
+}) {
+    return (
+        <div>
+        <ul class="nav nav-tabs" role="tablist">
+            <li class="nav-item" role="presentation">
+                <a class="nav-link active" data-bs-toggle="tab" href="#home" aria-selected="true" role="tab">Home</a>
+            </li>
+            <li class="nav-item" role="presentation">
+                <a class="nav-link" data-bs-toggle="tab" href="#profile" aria-selected="false" tabindex="-1" role="tab">Profile</a>
+            </li>
+            <li class="nav-item" role="presentation">
+                <a class="nav-link disabled" href="#" aria-selected="false" tabindex="-1" role="tab">Disabled</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                <div class="dropdown-menu" style="">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Separated link</a>
+                </div>
+            </li>
+        </ul>
+        <div id="myTabContent" class="tab-content">
+                <div class="tab-pane fade" id="dropdown1">
+                    <p>Test test test 123.</p>
+                </div>
+            </div>
+            </div>
+    );
+}
