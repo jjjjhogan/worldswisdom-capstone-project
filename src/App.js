1e26948f022b066d980d331760c5c325ae9ffc7e
Bhyrce <119452606+bhy8888@users.noreply.github.com>
2023-03-14 12:57:38 -0700

    test

--- src/App.js
+++ src/App.js
@@ -3,15 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoadingIndicator from "./components/LoadingIndicator";
 import Landing from "./containers/Landing";
 import ErrorBoundary from "./containers/ErrorBoundary";
-import LoginPage from "./containers/LoginPage";
-import RegisterPage from "./containers/RegisterPage";
-import RegisterPage2 from "./containers/RegisterPage2";
-import RegisterPage3 from "./containers/RegisterPage3";
 
-
-
-
-import Question_post_container from "./containers/Question_post_container";
 function App() {
   const [isLoading, setIsLoading] = useState(false);
 
@@ -24,14 +16,8 @@ function App() {
           }
         >
           <div>
-            
-
             <Routes>
               <Route path={`${process.env.PUBLIC_URL}/`} element={<Landing/>}></Route>
-              <Route path={`${process.env.PUBLIC_URL}/login`} element={<LoginPage />} />
-              <Route path={`${process.env.PUBLIC_URL}/register`} element={<RegisterPage />} />
-              <Route path={`${process.env.PUBLIC_URL}/register2`} element={<RegisterPage2 />} />
-              <Route path={`${process.env.PUBLIC_URL}/register3`} element={<RegisterPage3 />} />
             </Routes>
             <LoadingIndicator loading={isLoading} />
           </div>
