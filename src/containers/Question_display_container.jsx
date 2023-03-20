8ac457e795089b37c1226d21c0acf5395efddd1c
Bhyrce <119452606+bhy8888@users.noreply.github.com>
2023-03-20 01:10:02 -0700

    questiondisplaypage

--- src/containers/Question_display_container.jsx
+++ src/containers/Question_display_container.jsx
@@ -1,12 +1,7 @@
 import React from "react";
 import Question_display from "../components/Question_display";
-import profile from "../components/profile.png";
-import ReactPlayer from 'react-player';
-
-
 export default function Question_display_container() {
-
-    return (
+  return (
       <div className="d-flex flex-column inner-height landing">
         <div className="container flex-grow-1 text-center d-flex flex-column justify-content-center">
           <div>
