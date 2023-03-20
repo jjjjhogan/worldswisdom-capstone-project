75862e1ea4263f2b4525ca97f4b440593f02f036
John Hogan <jjhogan@uci.edu>
2023-03-19 22:05:50 -0700

    answer display container fixes and css fixes

--- src/components/AnswerDisplay.jsx
+++ src/components/AnswerDisplay.jsx
@@ -4,20 +4,21 @@ import down from "../components/down.png";
 import profile from "../components/profile.png";
 import ReactPlayer from 'react-player';
 
-export default function QuestionAnswer( props ){
+export default function AnswerDisplay( props ){
     return(
-        <div className="answer">
-            <div className="card border-light mb-3">
+
+        <div className="bg-light border">
                 <div className="profimage">
                     <img src={profile} style={{width:"30px", height:"30px"}} alt="" />
                     <p className="card-text">Username</p>
                 </div>
+                <div className="vid-player">
                     <ReactPlayer url={process.env.REACT_APP_S3_BASE_PATH + props.url} controls={true}/>
+                </div>
                 <div className="reflect">
-                    <img src={up} style={{width:"15px", height:"15px"}} alt="" />
-                    <img src={down} style={{width:"15px", height:"15px"}} alt="" />
+                    <img src={up} style={{width:"25px", height:"25px"}} alt="" />
+                    <img src={down} style={{width:"25px", height:"25px"}} alt="" />
                 </div>
             </div>
-        </div>
     );
 };
